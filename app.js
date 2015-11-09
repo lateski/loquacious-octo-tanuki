6b4e1ca1b37eb595993e6ef77197d534eb858b65
lateski <lauri.vene@gmail.com>
2015-11-09 15:18:45 +0200

    Added bootstrap and made page for viewing single beer

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/beers.js
+++ routes/beers.js
@@ -14,20 +14,44 @@ var myLogger = function (req, res, next) {
 
 router.use(myLogger);
 
+router.param('beer', function(req, res,  next, id){
+  Beer.find({'name': id }, function(err, beer){
+    if(err) {
+      next(err);
+    } else if (beer) {
+      req.beer = beer[0];
+      next();
+    } else {
+      next(new Error('Failed to find BEER!'));
+    }
+  });
+});
+
+router.get('/:beer', function(req, res){
+  console.log(req.beer)
+  res.render('beer', {beer : req.beer})
+});
+
 router.get('/', function (req, res) {
   Beer.find( function (err, beers){
     if (err) return console.error(err);
     res.render('beerlist', { title: 'Oluet', beers: beers })
   });
-})
+});
 
 router.post('/', function (req, res) {
-  var newBeer = new Beer(req.body);
+  var newBeer;
+  console.log(req.body);
+  newBeer = new Beer(req.body);
+
   newBeer.date = Date.now();
   newBeer.save(function (err, newBeer){
-    if (err) console.log(err.errors.name.toString());
+    if (err) console.log(err.errors);
   });
-  res.send("POST beers!");
-})
+  Beer.find(function (err, beers){
+    if (err) return console.error(err);
+    res.render('beerlist', { title: 'Oluet', beers: beers })
+  });
+});
 
 module.exports = router;
